23c3a5ed9b6cab25653fff987ab07577fef70d88
José Francisco Ferreira Alves de Sousa <josesousa9000@gmail.com>
2016-02-25 15:53:17 +0000

    reads a file with a link in each line and downloads and converts each video to mp3 sequentialy

---
+++ index.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var ytdl = require('ytdl-core');
+var ffmpeg = require('fluent-ffmpeg');
+var _ = require('underscore');
+var escapeStringRegexp = require('escape-string-regexp');
+
+if (!fs.existsSync('./media')) {
+	fs.mkdirSync('./media');
+}
+
+//cli args
+var args = process.argv.slice(2);
+
+fs.readFile(args[0], 'utf8', function(err, data) {
+
+	if (err) {
+		if (err.code == 'ENOENT') {
+
+			console.log("File doesn't exist!");
+		}
+		console.log(err.message);
+
+	} else {
+
+		console.log(data.trim());
+		var links = _.uniq(data.trim().split("\n").map(function(val) {
+			return val.trim();
+		}));
+		for (var i = 0; i < links.length; i++) {
+			downloadUrl(links[i], i);
+		}
+	}
+
+});
+
+function downloadUrl(url, filename) {
+	try {
+		ytdl.getInfo(url, function(err, info) {
+
+			var video = ytdl.downloadFromInfo(info, url);
+
+			new ffmpeg({
+				source: video
+			}).toFormat('mp3').pipe(fs.createWriteStream('./media/' + filename + '.mp3'));
+
+			console.log('Downloading ' + escapeStringRegexp(info.title));
+
+		});
+	} catch (err) {
+		console.log(err.message);
+	}
+}
